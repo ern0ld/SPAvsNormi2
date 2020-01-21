9690dc4455c98d1c4b2278a86c17425c44c31324
ern0ld <epa.suvila@gmail.com>
2020-01-21 13:14:51 +0200

    "kommentointi"

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 function myFunction() {
-  //Moi
+  //Luodaan index.html-sivulla olevista näppäimistä muuttujat
     let x = document.getElementById("spaBtn");
   let y = document.getElementById("normiBtn");
+
+  //Luodaan uusi otsikkoelementti
   let headerElement = document.createElement("h1");
+  //Asetetaan otsikkoelementin teksti
   headerElement.innerText = "Tämä on toinen sivu";
   
-  document.querySelector('#spaBtn').innerText = 'Vaihdettu';
-  //location.replace("https://www.google.com")
-    x.disabled = true;
+  //Piilotetaan index.html näppäimet
     y.hidden = true;
   x.hidden = true;
+  //Lisätään luotu otsikkoelementti sivustolle
   document.body.appendChild(headerElement);
-  console.log(document.location); 
+ 
   }
