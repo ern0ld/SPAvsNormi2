e7e1eb597ec78ad029ed5d3cac6f6a67d6db0309
ern0ld <50329110+ern0ld@users.noreply.github.com>
2020-01-29 16:48:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ function myFunction() {
 document.body.appendChild(pageBtn);
 
   }
-pageBtn.onclick = function(){createObjectPgae()};
+pageBtn.onclick = function(){createObjectPage()};
 
 //pageBtn.addEventListener("click", createObjectPage());
 function createObjectPage(){
